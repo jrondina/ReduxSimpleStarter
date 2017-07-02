a0aa1cc29107b526f6ed9ab25dc15c8a3183704b
James <jamesrondina@gmail.com>
2017-07-02 01:10:31 -0400

    input handler functionality

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,10 +8,9 @@ import React, { Component } from "react";
 class SearchBar extends Component {
     render() {
         return (
-            <input />
-        );
-    };
-
+            <input onChange={(event) => (console.log(event.target.value))} />
+        )
+    }
 }
 
 export default SearchBar;
