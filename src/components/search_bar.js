19f1cb5cb30046bb33fdce2062fb5a7bc7b9b07a
James <jamesrondina@gmail.com>
2017-07-02 03:26:06 -0400

    input state change

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,9 +6,20 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { term: 'enter text' };
+    }
     render() {
         return (
-            <input onChange={(event) => (console.log(event.target.value))} />
+            <div>
+            <input
+                value = {this.state.term}
+                onChange={(event) => this.setState({ term: event.target.value})} />
+                <p>Value of the input: { this.state.term}</p>
+            </div>
         )
     }
 }
